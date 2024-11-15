207b41f571d27983304b65b7fc68fedb30a9cf9a
KARWEST <vadimskedov@gmail.com>
2024-11-16 01:40:24 +0300

    update file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+  Шкедов Вадим Владимирович
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
