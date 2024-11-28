76657fb2bee4b110df519ede693889cc846dd434
Вадим <61047409+KARWEST@users.noreply.github.com>
2024-11-28 10:05:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code updated 2
+          Шкедов Вадим Владимирович
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Лаораторная 5
+          Лабораторная 5
         </a>
       </header>
     </div>
