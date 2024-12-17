80486f9eb6f81cdba6f3b17b6639e2cd16ec31b6
Герман <61047409+KARWEST@users.noreply.github.com>
2024-12-17 16:01:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Шкедов Вадим Владимирович
+          Елизаров Герман Дмитриевич
         </p>
         <a
           className="App-link"
